c6edf3b555539368a135f7818c5d6fc104614bf7
Didier321 <borjadidierestiben@gmail.com>
2024-11-08 20:24:36 -0500

    download cv

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode:'class' ,
 	theme: {
-		extend: {},
+		extend: {
+			animation:{
+				'float': 'float 8s ease-in-out infinite',
+			},
+			keyframes:{
+				float: {
+					'0%, 100%': { transform: 'translateY(-50px)' },
+					'50%': { transform: 'translateY(50px)' },
+				},
+			}
+		},
 	},
 	plugins: [],
 }
