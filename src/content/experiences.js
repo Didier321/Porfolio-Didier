0f1f3aadd48e27db4956c98792a9951ae5aa013f
Didier321 <borjadidierestiben@gmail.com>
2024-11-10 16:18:15 -0500

    experience

---
+++ src/content/experiences.js
@@ -0,0 +1,57 @@
+export const experiences = [
+    {
+      company: "Be Bolder",
+      role: "Desarrollador Full Stack",
+      employmentPeriod: "Enero 2024 - Julio 2024",
+      roleDescription: "Como Desarrollador Full Stack, trabajé en el análisis de requerimientos, desarrollo de nuevos módulos Frontend y refactorización de Backend, enfocado en mejorar el flujo de tareas internas y la recolección de datos de empleados.",
+      keyResponsibilities: [
+        "Desarrollé módulos Frontend con React.js y Angular para mejorar la recolección de datos de empleados.",
+        "Implementé pruebas unitarias en el Backend con JUnit5 y Mockito, alcanzando un 70% de cobertura de código.",
+        "Colaboré con equipos multidisciplinarios bajo la metodología SCRUM, logrando entregas eficientes."
+      ],
+      achievements: [
+        "Aumenté la eficiencia del código en un 30% mediante refactorización y pruebas.",
+        "Logré una expansión del 45% en la capacidad de almacenamiento de datos al ajustar esquemas de base de datos."
+      ],
+      technologiesAndTools: [
+        "Java",
+        "Spring Boot",
+        "React.js",
+        "Next.js",
+        "Angular",
+        "SQL",
+        "GitLab",
+        "IntelliJ IDEA",
+        "Postman",
+        "SCRUM"
+      ]
+    },
+    {
+      company: "Be Bolder",
+      role: "Desarrollador Full Stack",
+      employmentPeriod: "Enero 2024 - Julio 2024",
+      roleDescription: "Como Desarrollador Full Stack, trabajé en el análisis de requerimientos, desarrollo de nuevos módulos Frontend y refactorización de Backend, enfocado en mejorar el flujo de tareas internas y la recolección de datos de empleados.",
+      keyResponsibilities: [
+        "Desarrollé módulos Frontend con React.js y Angular para mejorar la recolección de datos de empleados.",
+        "Implementé pruebas unitarias en el Backend con JUnit5 y Mockito, alcanzando un 70% de cobertura de código.",
+        "Colaboré con equipos multidisciplinarios bajo la metodología SCRUM, logrando entregas eficientes."
+      ],
+      achievements: [
+        "Aumenté la eficiencia del código en un 30% mediante refactorización y pruebas.",
+        "Logré una expansión del 45% en la capacidad de almacenamiento de datos al ajustar esquemas de base de datos."
+      ],
+      technologiesAndTools: [
+        "Java",
+        "Spring Boot",
+        "React.js",
+        "Next.js",
+        "Angular",
+        "SQL",
+        "GitLab",
+        "IntelliJ IDEA",
+        "Postman",
+        "SCRUM"
+      ]
+    }
+    
+    ]
