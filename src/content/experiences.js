86a3fdf0b7a32a72db06b03fe23e8b7dcb1e1217
Didier321 <borjadidierestiben@gmail.com>
2024-11-13 11:29:50 -0500

    form

--- src/content/experiences.js
+++ src/content/experiences.js
@@ -1,6 +1,7 @@
 export const experiences = [
     {
       company: "Be Bolder",
+      companyDescription: "Be Bolder es una empresa de desarrollo de software personalizado y consultoría de transformación digital que apoya su cadena de crecimiento de compañías de aviación o viajes para que se venda de manera más eficiente a través de diferentes canales.",
       role: "Desarrollador Full Stack",
       employmentPeriod: "Enero 2024 - Julio 2024",
       roleDescription: "Como Desarrollador Full Stack, trabajé en el análisis de requerimientos, desarrollo de nuevos módulos Frontend y refactorización de Backend, enfocado en mejorar el flujo de tareas internas y la recolección de datos de empleados.",
@@ -26,32 +27,5 @@ export const experiences = [
         "SCRUM"
       ]
     },
-    {
-      company: "Be Bolder",
-      role: "Desarrollador Full Stack",
-      employmentPeriod: "Enero 2024 - Julio 2024",
-      roleDescription: "Como Desarrollador Full Stack, trabajé en el análisis de requerimientos, desarrollo de nuevos módulos Frontend y refactorización de Backend, enfocado en mejorar el flujo de tareas internas y la recolección de datos de empleados.",
-      keyResponsibilities: [
-        "Desarrollé módulos Frontend con React.js y Angular para mejorar la recolección de datos de empleados.",
-        "Implementé pruebas unitarias en el Backend con JUnit5 y Mockito, alcanzando un 70% de cobertura de código.",
-        "Colaboré con equipos multidisciplinarios bajo la metodología SCRUM, logrando entregas eficientes."
-      ],
-      achievements: [
-        "Aumenté la eficiencia del código en un 30% mediante refactorización y pruebas.",
-        "Logré una expansión del 45% en la capacidad de almacenamiento de datos al ajustar esquemas de base de datos."
-      ],
-      technologiesAndTools: [
-        "Java",
-        "Spring Boot",
-        "React.js",
-        "Next.js",
-        "Angular",
-        "SQL",
-        "GitLab",
-        "IntelliJ IDEA",
-        "Postman",
-        "SCRUM"
-      ]
-    }
     
     ]
